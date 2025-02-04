9917811b8c96464e52105f4c72d6bcbe9c53546e
jovan <jovanpaloski@gmail.com>
2025-02-04 23:05:48 +0100

    Add search by name

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -5,7 +5,8 @@ import movieService from "../services/movie-service.js";
 const movieController = Router();
 
 movieController.get('/search', (req, res) => {
-    const movies = movieService.getAll();
+    const filter = req.query;
+    const movies = movieService.getAll(filter);
 
     res.render('search', {movies});
 });
--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -2,8 +2,14 @@ import movies from "../movies.js";
 import { v4 as uuid } from 'uuid';
 
 export default {
-    getAll() {
-        return movies;
+    getAll(filter = {}) {
+        let result = movies;
+
+        if (filter.search) {
+            result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
+        }
+
+        return result;
     },
     findOne(movieId) {
         const result = movies.find(movie => movie.id === movieId);
