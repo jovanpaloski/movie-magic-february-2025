e4fa03bd79e08dc289b39bd4b67359ceac6363fb
jovan <jovanpaloski@gmail.com>
2025-02-04 19:26:50 +0100

    Add movie service

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import movieService from "../services/movie-service.js";
+
 
 const movieController = Router();
 
@@ -7,6 +9,13 @@ movieController.get('/create', (req, res) => {
 });
 
 movieController.get('/:movieId/details', (req, res) => {
+    const movieId = req.params.movieId;
+
+    console.log(movieId);
+
+    const movie = movieService.findOne(movieId);
+    console.log(movie);
+
     res.render('details');
 });
 
