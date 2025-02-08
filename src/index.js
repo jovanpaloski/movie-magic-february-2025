5b40505281af5a64dfe4553bef953541312a939c
jovan <jovanpaloski@gmail.com>
2025-02-08 20:01:30 +0100

    Setup mongoose

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+
 import routes from './routes.js'
 import showRatingHelper from './helpers/rating-helper.js';
+import mongoose from 'mongoose';
 
 const app = express();
+
+try {
+    const uri = 'mongodb://localhost:27017/magic-movies-feb2025';
+    await mongoose.connect(uri);
+
+    console.log('DB Connected Successfully');
+} catch (error) {
+    console.log('Cannot connect to DB');
+    console.error(error.message);
+}
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
